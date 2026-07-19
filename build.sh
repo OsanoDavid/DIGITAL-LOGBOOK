f621962415b5bb356daaaf3e912aff2b9429b165
OsanoDavid <osano@example.com>
2026-07-19 21:06:33 +0300

    Switch Render deployment to SQLite persistence

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -e
+mkdir -p /var/data
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
-python manage.py migrate
+python manage.py migrate --noinput
